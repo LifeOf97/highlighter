04c47d11f1da5acbc953f7988b117513c90c43fa
KMA-USS <koolkatswerve@gmail.com>
2020-10-06 13:45:30 +0100

    Updated urls file

--- backend/highlight/urls.py
+++ backend/highlight/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 appname = 'highlight'
 
-# router = routers.SimpleRouter()
-# router.register(r'highlighter', views.Highlighter, basename='highlighter')
-
 urlpatterns = [
     path('api/', include([
         path('highlighter/', views.Highlighter.as_view(), name='highliter'),
