86d7610496e749012fb019c464ddfc21eee023e6
realestkma <kelvinmayoayeni@gmail.com>
2022-08-10 12:56:22 +0100

    Updated settings

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,14 +1,14 @@
-import multiprocessing
+# import multiprocessing
 
-bind = "0.0.0.0:8000"
-# bind = "unix:/run/justhighlight.sock"
-workers = multiprocessing.cpu_count() * 2 + 1
-reload_engine = "inotify"
-group = "justhighlight"
-user = "justhighlight"
-reload = True
+# bind = "0.0.0.0:8000"
+# # bind = "unix:/run/justhighlight.sock"
+# workers = multiprocessing.cpu_count() * 2 + 1
+# reload_engine = "inotify"
+# group = "justhighlight"
+# user = "justhighlight"
+# reload = True
 
-# logs
-loglevel = "debug"
-accesslog = "logs/gunicorn/justhighlight.access"
-errorlog = "logs/gunicorn/justhighlight.error"
+# # logs
+# loglevel = "debug"
+# accesslog = "logs/gunicorn/justhighlight.access"
+# errorlog = "logs/gunicorn/justhighlight.error"
